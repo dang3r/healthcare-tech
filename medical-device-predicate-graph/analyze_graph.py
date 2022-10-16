437200a974ac4480ad8c48f8b139d542d7e643d4
Daniel Cardoza <dan@danielcardoza.com>
2022-10-16 18:05:26 -0400

    Generate device graph in CI

---
+++ medical-device-predicate-graph/analyze_graph.py
@@ -0,0 +1,22 @@
+import json
+
+import networkx as nx
+
+
+def get_cycles(g: nx.DiGraph) -> None:
+    cycles = list(nx.simple_cycles(g))
+    for cycle in cycles:
+        for node in cycle:
+            print("\t {}: {}".format(node, [str(x) for x in g.out_edges(node)]))
+
+
+if __name__ == "__main__":
+    g = nx.DiGraph()
+    with open("edges.txt") as f:
+        for line in f.readlines():
+            device, pred = line.strip().split(",")
+            g.add_edge(pred, device)
+
+    data = nx.node_link_data(g)
+    with open("graph.json", "w") as f:
+        f.write(json.dumps(data))
